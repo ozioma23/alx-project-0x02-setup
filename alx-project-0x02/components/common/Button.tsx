eff34c247ed82e075d4142a166fe254d21da297e
OgborFaith <faithogbor23@gmail.com>
2025-11-19 22:31:26 +0100

    Updated component UI and fixed navigation issues

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ size = "medium", shape = "rounded-md", children, onClick }) => {
+  let sizeClasses = "";
+  switch (size) {
+    case "small":
+      sizeClasses = "px-3 py-1 text-sm";
+      break;
+    case "medium":
+      sizeClasses = "px-4 py-2 text-base";
+      break;
+    case "large":
+      sizeClasses = "px-6 py-3 text-lg";
+      break;
+  }
+
+  return (
+    <button
+      className={`bg-blue-500 text-white ${shape} ${sizeClasses} hover:bg-blue-600 transition`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -2,6 +2,14 @@ const About = () => {
     return (
 <div>
      <h1 className="text-2xl font-semibold">About Page</h1>
-</div>
+      <div className="flex gap-4">
+        <Button size="small" shape="rounded-sm">Small</Button>
+        <Button size="medium" shape="rounded-md">Medium</Button>
+        <Button size="large" shape="rounded-full">Large</Button>
+      </div>
+    </div>
+
     )
 }
+
+export default About;

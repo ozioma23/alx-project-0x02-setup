86afa19ef9575ebabb44d6f09d6963b5f601d0c3
OgborFaith <faithogbor23@gmail.com>
2025-11-19 14:05:16 +0100

    Set up Next.js project with TypeScript, Tailwind and basic structure

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="w-full p-4 bg-gray-900 text-white">
+      <h1 className="text-2xl font-semibold">ALX Next.js Project</h1>
+    </header>
+    )
+}
+
+export default Header;

c163e752a6335a3754eadae580d79872c709a3fc
OgborFaith <faithogbor23@gmail.com>
2025-11-20 17:14:07 +0100

    corrections has been made to the post.tsx file

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,10 +1,46 @@
-import Header from "@/components/layout/Header";
+import Header from "../components/layout/Header";
+import PostCard from "../components/common/PostCard";
+import { PostProps } from "../interfaces";
 
-export default function Posts() {
+interface ApiPostType {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface PostsPageProps {
+  posts: ApiPostType[];
+}
+
+export default function Posts({ posts }: PostsPageProps) {
   return (
     <>
       <Header />
-      <h1>Posts Page</h1>
+      <div className="p-6">
+        <h1 className="text-2xl font-bold mb-4">Posts</h1>
+
+        {posts.map((post) => (
+          <PostCard
+            key={post.id}
+            title={post.title}
+            content={post.body}
+            userId={post.userId}
+          />
+        ))}
+      </div>
     </>
   );
+}
+
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return {
+    props: {
+      posts: data.slice(0, 10), 
+    },
+  };
 }
